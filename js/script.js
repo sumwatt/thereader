eea35cc2b4a0d93a6e7d72ad407e5088dbbdd24c
Bhawani Parajuli <parajulibhawani@gmail.com>
2016-06-23 14:26:45 -0700

    some modifications

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ $(function(){
     var link = $('#add-feed-link').val();
     var newSource = new Source(name, link);
     Reader.addSources(newSource);
-    $('#msg-box').text("Item added!").fadeToggle();
+    $('#msg-box').html("Item added!");
     $('#feed-list').empty();
     Reader.sources.forEach(function(source){
     renderFeedList(source);
